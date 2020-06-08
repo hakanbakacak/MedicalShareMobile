0fcc8058d1dc555cc74e442c220b6309023b572d
hakanbakacak <hakanbakacak98@gmail.com>
2020-06-08 23:52:21 +0300

    initialize

---
+++ android/app/src/main/kotlin/com/example/medical_share/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medical_share
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:medical_share/pages/homePage.dart';
+import 'package:medical_share/pages/notificationsPage.dart';
+import 'package:medical_share/pages/transactionHistoryPage.dart';
+import 'package:medical_share/pages/walletPage.dart';
+import 'view/bottombar.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MRM Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      //home: BottomBar(),
+      home: Scaffold(
+        body:BottomBar(),
+        /*floatingActionButton:Padding(
+          padding: const EdgeInsets.only(bottom: 90),
+          child: FloatingActionButton(child: Icon(Icons.attach_file, color: Colors.black,), onPressed: (){}, backgroundColor: Colors.white,),
+        )*/
+      ),
+    );
+  }
+}
+
+class BottomBar extends StatefulWidget {
+  final List<BarItem> barItems = [
+    BarItem(
+      text: "Anasayfa",
+      iconData: Icons.home,
+      color: Colors.indigo,
+    ),
+    BarItem(
+      text: "İşlemler",
+      iconData: Icons.loop,
+      color: Colors.purple,
+    ),
+    BarItem(
+      text: "Uyarılar",
+      iconData: Icons.notifications,
+      color: Color(0xff27496d),
+    ),
+    BarItem(
+      text: "Cüzdan",
+      iconData: Icons.account_balance_wallet,
+      color: Colors.teal,
+    ),
+  ];
+
+  @override
+  _BottomBarState createState() =>
+      _BottomBarState();
+}
+
+class _BottomBarState
+    extends State<BottomBar> {
+  int selectedBarIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedBarIndex) {
+      case 0:
+        page = HomePage();
+        break;
+      case 1:
+        page = TransactionHistory(); 
+        break;
+      case 2:
+        page = NotificationPage(widget.barItems[0].color);
+        break;
+      case 3: 
+        page = WalletPage();
+        break;
+      default:
+    }
+    return Scaffold(
+      body: AnimatedContainer(
+        color: widget.barItems[selectedBarIndex].color,
+        duration: const Duration(milliseconds: 300),
+        child: Center(child: page)
+      ),
+      bottomNavigationBar: AnimatedBottomBar(
+          barItems: widget.barItems,
+          animationDuration: const Duration(milliseconds: 150),
+          barStyle: BarStyle(
+            fontSize: 20.0,
+            iconSize: 30.0
+          ),
+          onBarTap: (index) {
+            setState(() {
+              selectedBarIndex = index;
+            });
+          }),
+    );
+  }
+}
